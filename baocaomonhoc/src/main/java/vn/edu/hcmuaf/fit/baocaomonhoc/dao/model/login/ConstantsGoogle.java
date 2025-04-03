ebc6ba74d43211d8b57b5f323a27fd26d7570cbc
DuyBao16 <22130020@st.hcmuaf.edu.vn>
2025-04-03 18:04:25 +0700

    google login

---
+++ baocaomonhoc/src/main/java/vn/edu/hcmuaf/fit/baocaomonhoc/dao/model/login/ConstantsGoogle.java
@@ -0,0 +1,10 @@
+package vn.edu.hcmuaf.fit.baocaomonhoc.dao.model.login;
+
+public class ConstantsGoogle {
+    public static String GOOGLE_CLIENT_ID = "326215351283-qci2bpspjmc6s664oi7tpamo3p0avqdm.apps.googleusercontent.com";
+    public static String GOOGLE_CLIENT_SECRET = "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";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/baocaomonhoc_war/loginGoogle";
+    public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}

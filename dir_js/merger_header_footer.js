c4d03ecea0908ee21e4bf8c788652b95bb8594e7
22130056-HoHoangDuy <22130056@st.hcmuaf.edu.vn>
2025-04-26 11:27:35 +0700

    src

--- dir_js/merger_header_footer.js
+++ dir_js/merger_header_footer.js
@@ -1,18 +1,7 @@
-// function loadHTML(id, url) {
-//     fetch(url)
-//         .then((response) => response.text())
-//         .then((data) => (document.getElementById(id).innerHTML = data))
-//         .catch((error) => console.error("Error:", error));
-// }
-
-// // Tải header và footer
-// loadHTML("header", "../dir_section/header.html");
-// loadHTML("footer", "../dir_section/footer.html");
 
 function loadHTML(selector, url) {
   fetch(url)
     .then((response) => {
-<<<<<<< HEAD
       if (!response.ok)
         throw new Error(`HTTP error! status: ${response.status}`);
       return response.text();
@@ -26,21 +15,4 @@ function loadHTML(selector, url) {
 // Tải header, body, và footer
 loadHTML("header", "../dir_section/header.html"); // File header
 loadHTML("footer", "../dir_section/footer.html"); // File footer
-=======
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      return response.text();
-    })
-    .then((data) => {
-      const element = document.querySelector(selector);
-      if (element) {
-        element.innerHTML = data;
-      } else {
-      }
-    })
-}
 
-// Gọi hàm
-loadHTML("#header", "../dir_section/header.html");
-loadHTML("#footer", "../dir_section/footer.html");
-
->>>>>>> ec004b6e2ad9031d77e0d24c106c72dce6feefaf
